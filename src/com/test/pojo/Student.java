59aa1206efdc33168902ebcda824d402a9acdd95
Yang <Yang@DESKTOP-BQPHGTA>
2018-03-03 15:57:00 +0800

    Test for Mybatis,such as CRUD,left join

--- src/com/test/pojo/Student.java
+++ src/com/test/pojo/Student.java
@@ -1,10 +1,12 @@
 package com.test.pojo;
 
+import java.util.List;
+
 public class Student {
 	private int id;
 	private String name;
 	private String sex;
-
+	List<Product> products;
 	public int getId() {
 		return id;
 	}
@@ -28,6 +30,16 @@ public class Student {
 	public void setSex(String sex) {
 		this.sex = sex;
 	}
+	public List<Product> getProducts() {
+        return products;
+    }
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+    @Override
+    public String toString() {
+        return "Category [id=" + id + ", name=" + name + "]";
+    }
 
 	
 
